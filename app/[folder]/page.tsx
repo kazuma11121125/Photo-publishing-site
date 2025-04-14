5c6856366c3e68eeab024450ce7a3892014c7627
kazuma1112 <kazuma1112.mainmail@gmail.com>
2025-04-14 20:28:06 +0900

    [fix] 次こそ

--- app/[folder]/page.tsx
+++ app/[folder]/page.tsx
@@ -1,5 +1,14 @@
+import fs from "fs";
+import path from "path";
 import Home from "../page";
 
-export default function FolderPage({ params }: { params: { folder: string } }) {
-    return <Home params={params} />;
+export default Home;
+
+export async function generateStaticParams() {
+    const publicPath = path.join(process.cwd(), "public");
+    const folders = fs.readdirSync(publicPath, { withFileTypes: true })
+        .filter((dirent) => dirent.isDirectory())
+        .map((dirent) => ({ folder: dirent.name }));
+
+    return folders;
 }
