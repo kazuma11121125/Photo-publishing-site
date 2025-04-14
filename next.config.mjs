cb762ee0ffe7b1ea4b9ea5ec1c45abe4114a2d11
kazuma1112 <kazuma1112.mainmail@gmail.com>
2025-04-14 21:36:16 +0900

    [fix] 404回避

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    images: {
+      unoptimized: true, // 静的エクスポート用に必須
+      
+    },
+    basePath: "/Photo-publishing-site",
+    assetPrefix: "/Photo-publishing-site",
+  };
+  
+  module.exports = nextConfig;
+  
